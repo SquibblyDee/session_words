a1c7831c5c79a75e0cfa08ef2d24a6187be1d497
unknown <steelst0@gmail.com>
2018-07-12 13:19:09 -0700

    routes set up form data is being passed to session

--- session_words/apps/session_words_app/urls.py
+++ session_words/apps/session_words_app/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
     url(r'^$', views.index),
-]
+    url(r'^process', views.process),
+    url(r'^clear', views.clear)
+]   
--- session_words/apps/session_words_app/views.py
+++ session_words/apps/session_words_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect
-
 # Create your views here.
 def index(request):
     return render(request,'session_words_app/index.html')
+
+def process(request, methods=['POST']):
+    request.session['word'] = request.POST['newword']
+    request.session['color'] = request.POST['radio']
+    request.session['size'] = request.POST['bigfont']
+    print("WORD: ",request.session['word'])
+    print("COLOR: ",request.session['color'])
+    print("BIG: ",request.session['size'])
+
+    return redirect('/')
+
+def clear(request):
+    request.session.clear()
+    return redirect('/')
